5a47c0cb3963f48a4fd917b9fdc111574316b920
Anikesh-Sharma <anikeshsharma319@gmail.com>
2025-02-19 13:58:09 +0530

    add

---
+++ FlashCard/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../App.css"
+
+const Card = () => {
+  return (
+    <div className='box-val'>
+        <h1>FlashCard App</h1> 
+        <div>
+            <h3>Question: <h4>what is the capital of france</h4></h3>
+        <h3>Answer: <h4>Paris</h4></h3>
+        </div>
+        <h1>FlashCard App</h1> 
+        <h3>Question: <h4>what is the capital of france</h4></h3>
+        <h3>Answer: <h4>Paris</h4></h3>
+        <h1>FlashCard App</h1> 
+        <h3>Question: <h4>what is the capital of france</h4></h3>
+        <h3>Answer: <h4>Paris</h4></h3>
+        <h1>FlashCard App</h1> 
+        <h3>Question: <h4>what is the capital of france</h4></h3>
+        <h3>Answer: <h4>Paris</h4></h3>
+    </div>
+  )
+}
+export default Card
